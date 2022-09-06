11cc801fa26e225164defb3edbfa9e9cb2a50afb
Aakarsh2000 <saiaakarshp@gmail.com>
2022-09-06 12:36:15 +0530

    Added useMemo in DemoUseMemo.js

--- src/components/DemoUseMemo.js
+++ src/components/DemoUseMemo.js
@@ -1,27 +1,40 @@
-import React,{useState} from 'react'
+import React,{useState, useMemo} from 'react'
 
 function DemoUseMemo() {
-    const [countOne,setCountOne]  = useState(0)
-    const incCounterOne = ()=>{
-        setCountOne(countOne+1)
-    }
-    const incCounterTwo =()=>{
-        setCountOne(countOne+2)
-    }
-    const checkForEven = () =>{
-        let i =0
-        while(i<3000000000) {
-            i++
-        }
-            return countOne % 2 == 0
-    }
-  return (
-    <div>DemoUseMemo
-        <button onClick={incCounterOne}>Increment one  -{countOne}</button>
-        
-        <span>{checkForEven() ? 'Even Number':'Odd Number'}</span>
 
-        <button onClick={incCounterTwo}>Increment Two  -{countOne}</button>
+    // useMemo
+    const [countOne, setCountOne] = useState(0)
+    const [countTwo, setCountTwo] = useState(0)
+
+    const incCounterOne = () => {
+        setCountOne(countOne + 1)
+    }
+
+    const incCounterTwo = () => {
+        setCountTwo(countTwo + 1)
+    }
+
+    const checkForEven = useMemo(
+        () => {
+            let i = 0
+            while( i < 3000000000) {
+                i++
+            }
+                
+                return countOne % 2 == 0
+            }, [countOne])       
+          
+       
+  return (
+    <div>DemoUseMemo <br></br><br></br>
+
+        <button onClick={incCounterOne}>INCREMENT one -   {countOne}</button> <br></br>
+         <span> {checkForEven ? 'Even Number' : 'Odd Number'}</span>
+
+        <br></br> <br></br>
+
+        <button onClick={incCounterTwo}>INCREMENT two -   {countTwo}</button>
+
     </div>
   )
 }
